158830643dc0f7337fb4b961c901be663eb68043
Abhinav <abhinavsinha135@gmail.com>
2024-12-04 22:18:36 +0530

    console added for env

--- index.ts
+++ index.ts
@@ -24,6 +24,8 @@ declare global {
   }
 }
 
+console.log(process.env.STRIPE_SECRET_KEY, "here")
+
 export const stripe = new Stripe(process.env.STRIPE_SECRET_KEY as string, {
   apiVersion: "2024-11-20.acacia",
 });
